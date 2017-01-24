ace546b410997c7bcf82701160729a4b45d297f0
Kunal Verma <kunalverma@Kunals-MacBook-Air.local>
2017-01-24 14:10:58 +0530

    Update

--- PowrBrowser/NetworkingClass.swift
+++ PowrBrowser/NetworkingClass.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import RealmSwift
 
 class NetworkingClass {
     
@@ -32,5 +33,53 @@ class NetworkingClass {
         }
         
         task.resume()
+    }
+}
+
+extension Date {
+    static let iso8601Formatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.calendar = Calendar(identifier: .iso8601)
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        formatter.timeZone = TimeZone(secondsFromGMT: 0)
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSXXXXX"
+        return formatter
+    }()
+    var iso8601: String {
+        return Date.iso8601Formatter.string(from: self)
+    }
+    static let iso8601LocalFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.calendar = Calendar(identifier: .iso8601)
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSXXXXX"
+        return formatter
+    }()
+    var localISO8601 : String {
+        print("LOCAL \(Date.iso8601LocalFormatter.string(from: self))")
+        return Date.iso8601LocalFormatter.string(from: self)
+    }
+}
+
+extension Object {
+    func toDictionary() -> NSDictionary {
+        let properties = self.objectSchema.properties.map { $0.name }
+        let dictionary = self.dictionaryWithValues(forKeys: properties)
+        let mutabledic = NSMutableDictionary()
+        mutabledic.setValuesForKeys(dictionary)
+        
+        for prop in self.objectSchema.properties as [Property]! {
+            // find lists
+            if let nestedObject = self[prop.name] as? Object {
+                mutabledic.setValue(nestedObject.toDictionary(), forKey: prop.name)
+            } else if let nestedListObject = self[prop.name] as? ListBase {
+                var objects = [AnyObject]()
+                for index in 0..<nestedListObject._rlmArray.count  {
+                    let object = nestedListObject._rlmArray[index] as AnyObject
+                    objects.append(object.toDictionary())
+                }
+                mutabledic.setObject(objects, forKey: prop.name as NSCopying)
+            }
+        }
+        return mutabledic
     }
 }
